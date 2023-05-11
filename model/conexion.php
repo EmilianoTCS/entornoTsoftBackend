3ae597bf4e2e0d9207cdf6ca340efed87595beee
EmilianoTCS <96253955+EmilianoTCS@users.noreply.github.com>
2023-05-11 17:23:35 -0300

    Conexion login BDD

---
+++ model/conexion.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'entornoTsoft';
+$conection = @mysqli_connect($host, $user, $password, $db);
+
+// $host = 'us-cdbr-east-06.cleardb.net';
+// $user = 'b2e8550b69d9c1';
+// $password = 'e2d710fc';
+// $db = 'heroku_a2bd4060d4a1323';
+// $conection = @mysqli_connect($host, $user, $password, $db);
+
+// if(!$conection){
+	// echo "Error en la conexión";
+// }else{
+	// echo "Conexión exitosa";
+// }
---
+++ model/login.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+include("conexion.php");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+if (isset($_GET['login'])) {
+    $data = json_decode(file_get_contents("php://input"));
+    $username = $data->username;
+    $password = $data->password;
+
+
+    function gToken($len)
+    {
+        $cadena = "_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
+        $tkn = "";
+        for ($i = 0; $i < $len; $i++) {
+            $tkn .= $cadena[rand(0, $len)];
+        }
+        return $tkn;
+    }
+    $token = gToken(40);
+
+    $query = "CALL SP_login('$username', '$password', @p1)";
+    $result = mysqli_query($conection, $query);
+    if (!$result) {
+        die('Query Failed' . mysqli_error($conection));
+    }
+    $row = mysqli_fetch_array($result);
+    $password_bd = $row['password'];
+    $pass_c = sha1($password);
+
+    if ($password_bd == $pass_c) {
+        $json[] = array(
+            'statusConected' => true,
+            'error' => false,
+            'token' => $token,
+            'usuario' => $row['usuario'],
+            'nomRol' => $row['nomRol'],
+
+        );
+        echo json_encode($json);
+    } else {
+        $json[] = array(
+            'statusConected' => false,
+            'token' => null,
+            'error' => true,
+            'usuario' => null,
+            'nomRol' => null,
+            'hola' => 'hola',
+
+
+        );
+        echo (json_encode($json));
+    }
+} else {
+    $json[] = array(
+        'statusConected' => false,
+        'token' => null,
+        'usuario' => null,
+        'error' => true,
+        'nomRol' => null,
+
+    );
+    echo (json_encode($json));
+}
