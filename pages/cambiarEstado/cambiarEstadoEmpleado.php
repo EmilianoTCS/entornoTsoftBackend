2025eaa3f64f0f9ad57eb3d699e1363af2869455
EmilianoTCS <96253955+EmilianoTCS@users.noreply.github.com>
2023-05-16 17:09:34 -0300

    editar empleado, cambiar estado empleado, listado auxiliar para forms added

---
+++ pages/cambiarEstado/cambiarEstadoEmpleado.php
@@ -0,0 +1,41 @@
+<?php
+
+include("../../model/conexion.php");
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if (isset($_GET['cambiarEstadoEmpleado'])) {
+    $data = json_decode(file_get_contents("php://input"));
+    $idEmpleado = $data->idEmpleado;
+    $usuarioModificacion = $data->usuarioModificacion;
+
+
+    $query = "CALL SP_cambiarEstadoEmpleado($idEmpleado,'$usuarioModificacion')";
+    $result = mysqli_query($conection, $query);
+    if (!$result) {
+        die('Query Failed' . mysqli_error($conection));
+    }
+
+    $json = array();
+    while ($row = mysqli_fetch_array($result)) {
+        $json[] = array(
+            'nomEmp' => $row['nomEmp'],
+            'correoEmp' => $row['correoEmp'],
+            'isActive' => $row['isActive'],
+            'nomCargo' => $row['nomCargo'],
+            'nomArea' => $row['nomArea'],
+            'fechaModificacion' => $row['fechaModificacion'],
+            'usuarioModificacion' => $row['usuarioModificacion'],
+        );
+    }
+    $jsonstring = json_encode($json);
+    echo $jsonstring;
+    mysqli_close($conection);
+
+} else {
+    echo json_encode("Error");
+}
--- pages/editar/editarEmpleado.php
+++ pages/editar/editarEmpleado.php
@@ -1,43 +1,26 @@
 <?php
 
 include("../../model/conexion.php");
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: GET,POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-if (isset($_GET['listadoEmpleados'])) {
+if (isset($_GET['editarEmpleado'])) {
     $data = json_decode(file_get_contents("php://input"));
-    $num_boton = $data->num_boton;
-    $cantidad_por_pagina = 6;
-    $inicio = ($num_boton - 1) * $cantidad_por_pagina;
+    $idEmpleado = $data->idEmpleado;
+    $nomEmpleado = $data->nomEmpleado;
+    $correoEmpleado = $data->correoEmpleado;
+    $telefonoEmpleado = $data->telefonoEmpleado;
+    $idPais = $data->idPais;
+    $idArea = $data->idArea;
+    $idCargo = $data->idCargo;
+    $usuarioModificacion = $data->usuarioModificacion;
 
-    $query = "CALL SP_listadoEmpleados('$inicio', '$cantidad_por_pagina')";
-    $result = mysqli_query($conection, $query);
-    if (!$result) {
-        die('Query Failed' . mysqli_error($conection));
-    }
 
-    $json = array();
-    while ($row = mysqli_fetch_array($result)) {
-        $json[] = array(
-            'idEmpleado' => $row['idEmpleado'],
-            'nomEmpleado' => $row['nomEmpleado'],
-            'correoEmpleado' => $row['correoEmpleado'],
-            'telefonoEmpleado' => $row['telefonoEmpleado'],
-            'nomArea' => $row['nomArea'],
-            'nomPais' => $row['nomPais'],
-            'nomCargo' => $row['nomCargo']
-        );
-    }
-    $jsonstring = json_encode($json);
-    echo $jsonstring;
-} else {
-    $cantidad_por_pagina = 6;
-    $inicio = 0;
-
-    $query = "CALL SP_listadoEmpleados('$inicio', '$cantidad_por_pagina')";
+    $query = "CALL SP_editarEmpleado($idEmpleado,'$nomEmpleado','$correoEmpleado','$telefonoEmpleado',$idPais,$idArea,$idCargo,'$usuarioModificacion')";
     $result = mysqli_query($conection, $query);
     if (!$result) {
         die('Query Failed' . mysqli_error($conection));
@@ -58,5 +41,6 @@ if (isset($_GET['listadoEmpleados'])) {
     $jsonstring = json_encode($json);
     echo $jsonstring;
     mysqli_close($conection);
-
+} else {
+    echo json_encode("Error");
 }
