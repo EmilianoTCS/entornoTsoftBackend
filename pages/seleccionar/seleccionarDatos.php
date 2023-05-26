01ce514b4388704165045e31be7460c59ddc8256
EmilianoTCS <96253955+EmilianoTCS@users.noreply.github.com>
2023-05-26 17:44:05 -0300

    fix codeResult

--- pages/seleccionar/seleccionarDatos.php
+++ pages/seleccionar/seleccionarDatos.php
@@ -21,137 +21,143 @@ if (isset($_GET['seleccionarDatos'])) {
 
     $json = array();
     while ($row = mysqli_fetch_array($result)) {
-
-        switch ($nombreTabla) {
-            case 'alumno':
-                $json[] = array(
-                    'idAlumno' => $row['idAlumno'],
-                    'nomAlumno' => $row['nomAlumno'],
-                    'correoAlumno' => $row['correoAlumno'],
-                    'telefonoAlumno' => $row['telefonoAlumno'],
-                    'nomArea' => $row['nomArea'],
-                    'nomServicio' => $row['nomServicio'],
-                    'nomPais' => $row['nomPais'],
-                    'nomCargo' => $row['nomCargo']
-                );
-                break;
-            case 'cliente':
-                $json[] = array(
-                    'idCliente' => $row['idCliente'],
-                    'nomCliente' => $row['UPPER(cli.nomCliente)'],
-                    'direccionCliente' => $row['UPPER(cli.direccionCliente)'],
-                    'nomPais' => $row['UPPER(pa.nomPais)'],
-                );
-                break;
-            case 'contacto':
-                $json[] = array(
-                    'idContacto' => $row['idContacto'],
-                    'nomContacto' => $row['UPPER(con.nomContacto)'],
-                    'correoContacto' => $row['UPPER(con.correoContacto)'],
-                    'fechaIni' => $row['fechaIni'],
-                    'fechaFin' => $row['fechaFin'],
-                    'nomServicio' => $row['UPPER(serv.nomServicio)']
-                );
-                break;
-            case 'curso':
-                $json[] = array(
-                    'idCurso' => $row['idCurso'],
-                    'codCurso' => $row['UPPER(cur.codCurso)'],
-                    'nomCurso' => $row['UPPER(cur.nomCurso)'],
-                    'tipoHH' => $row['UPPER(cur.tipoHH)'],
-                    'duracionCursoHH' => $row['duracionCursoHH'],
-                    'cantSesionesCurso' => $row['cantSesionesCurso']
-                );
-                break;
-            case 'cursoalumno':
-                $json[] = array(
-                    'idCursoAlumno' => $row['idCursoAlumno'],
-                    'fechaIni' => $row['fechaIni'],
-                    'horaIni' => $row['horaIni'],
-                    'fechaFin' => $row['fechaFin'],
-                    'horaFin' => $row['horaFin'],
-                    'porcAsistencia' => $row['porcAsistencia'],
-                    'porcParticipacion' => $row['porcParticipacion'],
-                    'claseAprobada' => $row['UPPER(curAl.claseAprobada)'],
-                    'porcAprobacion' => $row['porcAprobacion'],
-                    'estadoCurso' => $row['UPPER(curAl.estadoCurso)'],
-                    'nomAlumno' => $row['UPPER(al.nomAlumno)'],
-                    'nomCurso' => $row['UPPER(cur.nomCurso)']
-                );
-                break;
-            case 'cursoalumno_sesion':
-                $json[] = array(
-                    'idCursoAlumnoSesion' => $row['idCursoAlumnoSesion'],
-                    'fechaIni' => $row['fechaIni'],
-                    'fechaFin' => $row['fechaFin'],
-                    'asistencia' => $row['asistencia'],
-                    'participacion' => $row['participacion'],
-                    'nomSesion' => $row['UPPER(se.nomSesion)'],
-                    'idCursoAlumno' => $row['idCursoAlumno']
-                );
-                break;
-            case 'empleado':
-                $json[] = array(
-                    'idEmpleado' => $row['idEmpleado'],
-                    'nomEmpleado' => $row['UPPER(emp.nomEmpleado)'],
-                    'correoEmpleado' => $row['UPPER(emp.correoEmpleado)'],
-                    'telefonoEmpleado' => $row['telefonoEmpleado'],
-                    'nomArea' => $row['UPPER(ar.nomArea)'],
-                    'nomPais' => $row['UPPER(pa.nomPais)'],
-                    'nomCargo' => $row['UPPER(ca.nomCargo)']
-                );
-                break;
-            case 'notaexamen':
-                $json[] = array(
-                    'idNotaExamen' => $row['idNotaExamen'],
-                    'notaExamen' => $row['notaExamen'],
-                    'apruebaExamen' => $row['UPPER(notaEx.apruebaExamen)'],
-                    'nomExamen' => $row['UPPER(ramoEx.nomExamen)'],
-                    'idCursoAlumno' => $row['idCursoAlumno']
-                );
-                break;
-            case 'ramoexamen':
-                $json[] = array(
-                    'idRamoExamen' => $row['idRamoExamen'],
-                    'nomExamen' => $row['UPPER(ramEx.nomExamen)'],
-                    'fechaExamen' => $row['fechaExamen'],
-                    'nomRamo' => $row['UPPER(ram.nomRamo)']
-                );
-                break;
-            case 'relatorramo':
-                $json[] = array(
-                    'idRelatorRamo' => $row['idRelatorRamo'],
-                    'fechaIni' => $row['fechaIni'],
-                    'fechaFin' => $row['fechaFin'],
-                    'nomEmpleado' => $row['UPPER(emp.nomEmpleado)'],
-                    'nomRamo' => $row['UPPER(ram.nomRamo)']
-                );
-                break;
-            case 'reqcurso':
-                $json[] = array(
-                    'idReqCurso' => $row['idReqCurso'],
-                    'nomCurso' => $row['UPPER(cur.nomCurso)'],
-                    'requisitoCurso' => $row['requisitoCurso']
-                );
-                break;
-            case 'servicio':
-                $json[] = array(
-                    'idServicio' => $row['idServicio'],
-                    'nomServicio' => $row['UPPER(serv.nomServicio)'],
-                    'nomCliente' => $row['UPPER(cli.nomCliente)'],
-                );
-                break;
-            case 'sesion':
-                $json[] = array(
-                    'idSesion' => $row['idSesion'],
-                    'nroSesion' => $row['nroSesion'],
-                    'nomSesion' => $row['UPPER(se.nomSesion)'],
-                    'tipoSesion' => $row['UPPER(se.tipoSesion)'],
-                    'tipoSesionHH' => $row['UPPER(se.tipoSesionHH)'],
-                    'duracionSesionHH' => $row['UPPER(se.duracionSesionHH)'],
-                    'nomRamo' => $row['UPPER(ram.nomRamo)']
-                );
-                break;
+        if ($row['OUT_CODRESULT'] != '00') {
+            $json[] = array(
+                'OUT_CODRESULT' => $row['OUT_CODRESULT'],
+                'OUT_MJERESULT' => $row['OUT_MJERESULT']
+            );
+        } else {
+            switch ($nombreTabla) {
+                case 'alumno':
+                    $json[] = array(
+                        'idAlumno' => $row['idAlumno'],
+                        'nomAlumno' => $row['nomAlumno'],
+                        'correoAlumno' => $row['correoAlumno'],
+                        'telefonoAlumno' => $row['telefonoAlumno'],
+                        'nomArea' => $row['nomArea'],
+                        'nomServicio' => $row['nomServicio'],
+                        'nomPais' => $row['nomPais'],
+                        'nomCargo' => $row['nomCargo']
+                    );
+                    break;
+                case 'cliente':
+                    $json[] = array(
+                        'idCliente' => $row['idCliente'],
+                        'nomCliente' => $row['UPPER(cli.nomCliente)'],
+                        'direccionCliente' => $row['UPPER(cli.direccionCliente)'],
+                        'nomPais' => $row['UPPER(pa.nomPais)'],
+                    );
+                    break;
+                case 'contacto':
+                    $json[] = array(
+                        'idContacto' => $row['idContacto'],
+                        'nomContacto' => $row['UPPER(con.nomContacto)'],
+                        'correoContacto' => $row['UPPER(con.correoContacto)'],
+                        'fechaIni' => $row['fechaIni'],
+                        'fechaFin' => $row['fechaFin'],
+                        'nomServicio' => $row['UPPER(serv.nomServicio)']
+                    );
+                    break;
+                case 'curso':
+                    $json[] = array(
+                        'idCurso' => $row['idCurso'],
+                        'codCurso' => $row['UPPER(cur.codCurso)'],
+                        'nomCurso' => $row['UPPER(cur.nomCurso)'],
+                        'tipoHH' => $row['UPPER(cur.tipoHH)'],
+                        'duracionCursoHH' => $row['duracionCursoHH'],
+                        'cantSesionesCurso' => $row['cantSesionesCurso']
+                    );
+                    break;
+                case 'cursoalumno':
+                    $json[] = array(
+                        'idCursoAlumno' => $row['idCursoAlumno'],
+                        'fechaIni' => $row['fechaIni'],
+                        'horaIni' => $row['horaIni'],
+                        'fechaFin' => $row['fechaFin'],
+                        'horaFin' => $row['horaFin'],
+                        'porcAsistencia' => $row['porcAsistencia'],
+                        'porcParticipacion' => $row['porcParticipacion'],
+                        'claseAprobada' => $row['UPPER(curAl.claseAprobada)'],
+                        'porcAprobacion' => $row['porcAprobacion'],
+                        'estadoCurso' => $row['UPPER(curAl.estadoCurso)'],
+                        'nomAlumno' => $row['UPPER(al.nomAlumno)'],
+                        'nomCurso' => $row['UPPER(cur.nomCurso)']
+                    );
+                    break;
+                case 'cursoalumno_sesion':
+                    $json[] = array(
+                        'idCursoAlumnoSesion' => $row['idCursoAlumnoSesion'],
+                        'fechaIni' => $row['fechaIni'],
+                        'fechaFin' => $row['fechaFin'],
+                        'asistencia' => $row['asistencia'],
+                        'participacion' => $row['participacion'],
+                        'nomSesion' => $row['UPPER(se.nomSesion)'],
+                        'idCursoAlumno' => $row['idCursoAlumno']
+                    );
+                    break;
+                case 'empleado':
+                    $json[] = array(
+                        'idEmpleado' => $row['idEmpleado'],
+                        'nomEmpleado' => $row['UPPER(emp.nomEmpleado)'],
+                        'correoEmpleado' => $row['UPPER(emp.correoEmpleado)'],
+                        'telefonoEmpleado' => $row['telefonoEmpleado'],
+                        'nomArea' => $row['UPPER(ar.nomArea)'],
+                        'nomPais' => $row['UPPER(pa.nomPais)'],
+                        'nomCargo' => $row['UPPER(ca.nomCargo)']
+                    );
+                    break;
+                case 'notaexamen':
+                    $json[] = array(
+                        'idNotaExamen' => $row['idNotaExamen'],
+                        'notaExamen' => $row['notaExamen'],
+                        'apruebaExamen' => $row['UPPER(notaEx.apruebaExamen)'],
+                        'nomExamen' => $row['UPPER(ramoEx.nomExamen)'],
+                        'idCursoAlumno' => $row['idCursoAlumno']
+                    );
+                    break;
+                case 'ramoexamen':
+                    $json[] = array(
+                        'idRamoExamen' => $row['idRamoExamen'],
+                        'nomExamen' => $row['UPPER(ramEx.nomExamen)'],
+                        'fechaExamen' => $row['fechaExamen'],
+                        'nomRamo' => $row['UPPER(ram.nomRamo)']
+                    );
+                    break;
+                case 'relatorramo':
+                    $json[] = array(
+                        'idRelatorRamo' => $row['idRelatorRamo'],
+                        'fechaIni' => $row['fechaIni'],
+                        'fechaFin' => $row['fechaFin'],
+                        'nomEmpleado' => $row['UPPER(emp.nomEmpleado)'],
+                        'nomRamo' => $row['UPPER(ram.nomRamo)']
+                    );
+                    break;
+                case 'reqcurso':
+                    $json[] = array(
+                        'idReqCurso' => $row['idReqCurso'],
+                        'nomCurso' => $row['UPPER(cur.nomCurso)'],
+                        'requisitoCurso' => $row['requisitoCurso']
+                    );
+                    break;
+                case 'servicio':
+                    $json[] = array(
+                        'idServicio' => $row['idServicio'],
+                        'nomServicio' => $row['UPPER(serv.nomServicio)'],
+                        'nomCliente' => $row['UPPER(cli.nomCliente)'],
+                    );
+                    break;
+                case 'sesion':
+                    $json[] = array(
+                        'idSesion' => $row['idSesion'],
+                        'nroSesion' => $row['nroSesion'],
+                        'nomSesion' => $row['UPPER(se.nomSesion)'],
+                        'tipoSesion' => $row['UPPER(se.tipoSesion)'],
+                        'tipoSesionHH' => $row['UPPER(se.tipoSesionHH)'],
+                        'duracionSesionHH' => $row['UPPER(se.duracionSesionHH)'],
+                        'nomRamo' => $row['UPPER(ram.nomRamo)']
+                    );
+                    break;
+            }
         }
     }
 
