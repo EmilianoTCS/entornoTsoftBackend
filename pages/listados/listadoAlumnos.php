3c329accb77f31538cec6cfc46abc8612cb2eafc
EmilianoTCS <emi.vega84@gmail.com>
2023-06-14 16:22:08 -0300

    hot fix listadoAlumno

--- pages/listados/listadoAlumnos.php
+++ pages/listados/listadoAlumnos.php
@@ -12,7 +12,7 @@ if (isset($_GET['listadoAlumnos'])) {
 
     $data = json_decode(file_get_contents("php://input"));
     $data->num_boton = "" || null ? $num_boton = 1 : $num_boton = $data->num_boton;
-    $data->idPais = "" || null ? $idPais = null : $idPais = $data->num_boidPaiston;
+    $data->idPais = "" || null ? $idPais = null : $idPais = $data->idPais;
     $data->idServicio = "" || null ? $idServicio = null : $idServicio = $data->idServicio;
     $data->idArea = "" || null ? $idArea = null : $idArea = $data->idArea;
     $data->idCargo = "" || null ? $idCargo = null : $idCargo = $data->idCargo;
