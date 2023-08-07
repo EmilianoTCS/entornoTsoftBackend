12cd6de8e17f907836a110c5ef28081ec77f05e7
EmilianoTCS <emi.vega84@gmail.com>
2023-08-07 17:19:04 -0300

    added listado respuestas finales

---
+++ pages/listados/listadoProyRespFinales.php
@@ -0,0 +1,46 @@
+<?php
+
+include("../../model/conexion.php");
+include("../paginador/cantPaginas.php");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET,POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+if (isset($_GET['listadoProyRespFinales'])) {
+    $data = json_decode(file_get_contents("php://input"));
+    $idEvaluacion = $data->idEvaluacion;
+    $idEmpleado = $data->idEmpleado;
+    $idEDDProyEmpEvaluado = $data->idEDDProyEmpEvaluado;
+
+    $query = "CALL SP_listadoProyRespFinales('$idEvaluacion', '$idEmpleado', '$idEDDProyEmpEvaluado', @p0, @p1)";
+    $result = mysqli_query($conection, $query);
+    if (!$result) {
+        die('Query Failed' . mysqli_error($conection));
+    }
+
+    $json = array();
+    while ($row = mysqli_fetch_array($result)) {
+        if ($row['OUT_CODRESULT'] != '00') {
+            $json[] = array(
+                'OUT_CODRESULT' => $row['OUT_CODRESULT'],
+                'OUT_MJERESULT' => $row['OUT_MJERESULT']
+            );
+        } else {
+            $json[] = array(
+                'idEDDEvalProyResp' => $row['idEDDEvalProyResp'],
+                'idEDDEvaluacion' => $row['idEDDEvaluacion'],
+                'idEDDProyEmp' => $row['idEDDProyEmp'],
+                'respuesta' => $row['respuesta'],
+                'idEDDEvalProyEmp' => $row['idEDDEvalProyEmp'],
+                'idEDDEvalRespPreg' => $row['idEDDEvalRespPreg'],
+                'pregunta' => $row['pregunta'],
+                'ordenPregunta' => $row['ordenPregunta']
+            );
+        }
+    }
+    $jsonstring = json_encode(($json));
+    echo $jsonstring;
+    mysqli_close($conection);
+}
