092f3d70317791c2afac40ec176534b7d6a6b6b2
EmilianoTCS <emi.vega84@gmail.com>
2024-11-25 11:29:38 -0300

    versión funcional 25-11-2024

--- pages/listados/listadoRelatorRamo.php
+++ pages/listados/listadoRelatorRamo.php
@@ -14,11 +14,21 @@ if (isset($_GET['listadoRelatorRamo'])) {
     $data->num_boton = "" || null ? $num_boton = 1 : $num_boton = $data->num_boton;
     $data->idEmpleado = "" || null ? $idEmpleado = null : $idEmpleado = $data->idEmpleado;
     $data->idRamo = "" || null ? $idRamo = null : $idRamo = $data->idRamo;
+    $isActive = (!isset($data->isActive) || $data->isActive === '' || $data->isActive === null) ? null : $data->isActive;
+
     $data->cantidadPorPagina = "" || null ? $cantidadPorPagina = 10 : $cantidadPorPagina = $data->cantidadPorPagina;
     $inicio = ($num_boton - 1) * $cantidadPorPagina;
 
+    // echo json_encode($isActive);    
+    $query = "CALL SP_listadoRelatorRamo(
+        '" . $inicio . "', 
+        '" . $cantidadPorPagina . "', 
+        " . ($idEmpleado === '' || $idEmpleado === null ? "NULL" : "'" . $idEmpleado . "'") . ", 
+        " . ($idRamo === '' || $idRamo === null ? "NULL" : "'" . $idRamo . "'") . ", 
+        " . ($isActive === '' || $isActive === null ? "NULL" : "'" . $isActive . "'") . 
+    ")";
 
-    $query = "CALL SP_listadoRelatorRamo('$inicio', '$cantidadPorPagina', '$idEmpleado', '$idRamo')";
+    // echo json_encode($query);
     $result = mysqli_query($conection, $query);
     if (!$result) {
         die('Query Failed' . mysqli_error($conection));
@@ -27,37 +37,37 @@ if (isset($_GET['listadoRelatorRamo'])) {
     $json = array();
     if (mysqli_num_rows($result) > 0) {
 
-    while ($row = mysqli_fetch_array($result)) {
-        $json[] = array(
-            'idRelatorRamo' => $row['idRelatorRamo'],
-            'fechaIni' => $row['fechaIni'],
-            'fechaFin' => $row['fechaFin'],
-            'nomEmpleado' => $row['UPPER(emp.nomEmpleado)'],
-            'nomRamo' => $row['UPPER(ram.nomRamo)']
-        );
+        while ($row = mysqli_fetch_array($result)) {
+            $json[] = array(
+                'idRelatorRamo' => $row['idRelatorRamo'],
+                'fechaIni' => $row['fechaIni'],
+                'fechaFin' => $row['fechaFin'],
+                'nomEmpleado' => $row['nomEmpleado'],
+                'nomRamo' => $row['nomRamo'],
+                'isActive' => $row['isActive'],
+            );
             $FN_cantPaginas = cantPaginas($row['@temp_cantRegistros'], $cantidadPorPagina);
-      
+        }
+        $jsonstring = json_encode([
+            'datos' => $json,
+            'paginador' => $FN_cantPaginas
+        ]);
+        echo $jsonstring;
+    } else {
+        $json[] = array(
+            'idRelatorRamo' => 'empty / vacio',
+            'fechaIni' => 'empty / vacio',
+            'fechaFin' => 'empty / vacio',
+            'nomEmpleado' => 'empty / vacio',
+            'nomRamo' => 'empty / vacio',
+
+        );
+
+        $FN_cantPaginas = cantPaginas(1, $cantidadPorPagina);
+        $jsonstring = json_encode([
+            'datos' => $json,
+            'paginador' => $FN_cantPaginas
+        ]);
+        echo $jsonstring;
     }
-    $jsonstring = json_encode([
-        'datos' => $json,
-        'paginador' => $FN_cantPaginas
-    ]);
-    echo $jsonstring;
-} else {
-    $json[] = array(
-        'idRelatorRamo' => 'empty / vacio',
-        'fechaIni' => 'empty / vacio',
-        'fechaFin' => 'empty / vacio',
-        'nomEmpleado' => 'empty / vacio',
-        'nomRamo' => 'empty / vacio',
-
-    );
-
-    $FN_cantPaginas = cantPaginas(1, $cantidadPorPagina);
-    $jsonstring = json_encode([
-        'datos' => $json,
-        'paginador' => $FN_cantPaginas
-    ]);
-    echo $jsonstring;
-}
 }
